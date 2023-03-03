45cf7cbd31c19044800ec79ee511ae3cfcb08b05
Roger Steve Ruiz <hi@rog.gr>
2023-03-03 00:42:23 -0500

    Compromiso inicial de mi nuevo blog;
    Estoy usando a Hugo con Tailwind CSS en este proyecto.

---
+++ README.md
@@ -0,0 +1,9 @@
+# 2023 Redesign
+
+This is the `write.rog.gr` website re-designed for 2023. The original design was
+created in 2015 and with Jekyll rather than Hugo. Lots of things have changed
+then, so I decided that my site will need to change as well.
+
+This version of the site contains all the original posts from the 2015 design
+going forward. Some posts will be modified for clarity or accuracy, but
+otherwise the content is the same.
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["themes/2023-rogeruiz/**/*.html", "./content/**/*.{html,js}", "./layouts/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+        mono: ['mplus-2m-nerd-font-patched', ...defaultTheme.fontFamily.mono],
+      }
+    },
+  },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      defaultFlavour: 'latte',
+    }),
+  ],
+};
